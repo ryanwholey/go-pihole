2fba11700e096f8fdfdfd8707bbe63462f2d728a
Ryan Wholey <ryan@takescoop.com>
2022-02-19 12:20:46 -0700

    Add cname acc tests (#4)

---
+++ local_cname_test.go
@@ -0,0 +1,94 @@
+package pihole
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func testAssertCNAME(t *testing.T, c Client, expected *CNAMERecord, assertErr error) {
+	actual, err := c.LocalCNAME.Get(context.TODO(), expected.Domain)
+	if assertErr != nil {
+		assert.ErrorAs(t, err, assertErr)
+		return
+	}
+
+	require.NoError(t, err)
+
+	assert.Equal(t, expected.Domain, actual.Domain)
+	assert.Equal(t, expected.Target, actual.Target)
+}
+
+func cleanupCNAME(t *testing.T, c Client, domain string) {
+	if err := c.LocalCNAME.Delete(context.TODO(), domain); err != nil {
+		log.Printf("Failed to clean up CNAME record: %s\n", domain)
+	}
+}
+
+func TestLocalCNAME(t *testing.T) {
+	t.Run("Test create a CNAME record", func(t *testing.T) {
+		isAcceptance(t)
+		accPreflghtCheck(t)
+
+		c := newTestClient()
+
+		domain := fmt.Sprintf("test.%s", randomID())
+
+		record, err := c.LocalCNAME.Create(context.Background(), domain, "domain.com")
+		require.NoError(t, err)
+
+		defer cleanupCNAME(t, c, domain)
+
+		testAssertCNAME(t, c, record, nil)
+		testAssertCNAME(t, c, &CNAMERecord{
+			Domain: record.Domain,
+			Target: "domain.com",
+		}, nil)
+	})
+
+	t.Run("Test update a CNAME record", func(t *testing.T) {
+		isAcceptance(t)
+		accPreflghtCheck(t)
+
+		c := newTestClient()
+
+		domain := fmt.Sprintf("test.%s", randomID())
+
+		record, err := c.LocalCNAME.Create(context.Background(), domain, "domain.com")
+		require.NoError(t, err)
+		defer cleanupCNAME(t, c, record.Domain)
+
+		updated, err := c.LocalCNAME.Update(context.Background(), record.Domain, "domain-2.com")
+		require.NoError(t, err)
+
+		testAssertCNAME(t, c, updated, nil)
+		testAssertCNAME(t, c, &CNAMERecord{
+			Domain: record.Domain,
+			Target: "domain-2.com",
+		}, nil)
+	})
+
+	t.Run("Test delete a CNAME record", func(t *testing.T) {
+		isAcceptance(t)
+		accPreflghtCheck(t)
+
+		c := newTestClient()
+		ctx := context.Background()
+
+		domain := fmt.Sprintf("test.%s", randomID())
+
+		record, err := c.LocalCNAME.Create(ctx, domain, "domain.com")
+		require.NoError(t, err)
+		defer cleanupCNAME(t, c, record.Domain)
+
+		err = c.LocalCNAME.Delete(ctx, domain)
+		require.NoError(t, err)
+
+		_, err = c.LocalCNAME.Get(ctx, domain)
+		assert.ErrorIs(t, err, ErrorLocalCNAMENotFound)
+	})
+}
